19e43eab87277fdeeb448f33996731923d4d50f1
Chris Rebert <code@rebertia.com>
2014-07-11 01:23:19 -0700

    fix #4

--- src/bootlint.js
+++ src/bootlint.js
@@ -4,12 +4,27 @@
  * Licensed under the MIT license.
  */
 
-(function(exports) {
+(function (exports) {
+    'use strict';
 
-  'use strict';
-
-  exports.awesome = function() {
-    return 'awesome';
-  };
+    exports.lintMetaCharsetUtf8 = function ($) {
+        var meta = $('head>meta[charset]');
+        var charset = meta.attr('charset');
+        if (!charset) {
+            return '<head> is missing UTF-8 charset <meta> tag';
+        }
+        if (charset.toLowerCase() !== "utf-8") {
+            return 'charset meta tag is specifying a legacy, non-UTF-8 charset';
+        }
+        return null;
+    };
+    exports.lint = function (html) {
+        var cheerio = require('cheerio');
+        var $ = cheerio.load(html);
+        var errs = [];
+        errs.push(this.lintMetaCharsetUtf8($));
+        errs = errs.filter(function (item) { return item !== null; });
+        return errs;
+    };
 
 }(typeof exports === 'object' && exports || this));
--- test/bootlint_test.js
+++ test/bootlint_test.js
@@ -1,36 +1,50 @@
 'use strict';
 
 var bootlint = require('../src/bootlint.js');
-
+var fs = require('fs');
+function _fixtureNameToFilepath(name) {
+    return __dirname + '/fixtures/' + name;
+}
+function utf8Fixture(name) {
+    return fs.readFileSync(_fixtureNameToFilepath(name), {encoding: 'utf8'});
+}
+function utf16Fixture(name) {
+    return fs.readFileSync(_fixtureNameToFilepath(name), {encoding: 'utf16le'});
+}
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+    ======== A Handy Little Nodeunit Reference ========
+    https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+    Test methods:
+        test.expect(numAssertions)
+        test.done()
+    Test assertions:
+        test.ok(value, [message])
+        test.deepEqual(actual, expected, [message])
+        test.notDeepEqual(actual, expected, [message])
+        test.strictEqual(actual, expected, [message])
+        test.notStrictEqual(actual, expected, [message])
+        test.throws(block, [error], [message])
+        test.doesNotThrow(block, [error], [message])
+        test.ifError(value)
 */
 
 exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(bootlint.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
+    setUp: function(done) {
+        // setup here
+        done();
+    },
+    'UTF-8 charset meta tag': function (test) {
+        test.expect(3);
+        test.deepEqual(bootlint.lint(utf8Fixture('charset/utf8.html')),
+            [],
+            'should not complain when UTF-8 charset meta tag is present.');
+        test.deepEqual(bootlint.lint(utf8Fixture('charset/missing.html')),
+            ['<head> is missing UTF-8 charset <meta> tag'],
+            'should complain when charset meta tag is missing.');
+        test.deepEqual(bootlint.lint(utf16Fixture('charset/not-utf8.html')),
+            ['charset meta tag is specifying a legacy, non-UTF-8 charset'],
+            'should complain when meta tag specifies non-UTF-8 charset.');
+        test.done();
+    }
 };
