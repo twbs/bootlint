b96b1674af1022fb2afd92ec9793c7c77bb11be7
Chris Rebert <code@rebertia.com>
2014-07-10 22:55:25 -0700

    add stub source via yeoman

---
+++ src/bootlint.js
@@ -0,0 +1,15 @@
+/*!
+ * bootlint - https://github.com/cvrebert/bootlint
+ * Copyright (c) 2014 Christopher Rebert
+ * Licensed under the MIT license.
+ */
+
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
