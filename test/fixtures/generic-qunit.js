3807cbf6114075dc8ed7e8feca48421f94e643f3
Chris Rebert <code@rebertia.com>
2014-07-20 19:44:32 -0700

    add generic QUnit test script

---
+++ test/fixtures/generic-qunit.js
@@ -0,0 +1,7 @@
+QUnit.test(window.location.pathname, function (assert) {
+    expect(1);
+    var lints = Array.prototype.slice.call(document.querySelectorAll('#bootlint>li'));
+    var expectedLintMsgs = lints.map(function (item) { return item.dataset.lint; });
+    var actualLintMsgs = bootlint.lintCurrentDocument();
+    assert.deepEqual(actualLintMsgs, expectedLintMsgs);
+});
