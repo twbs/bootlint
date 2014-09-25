e686efc9ccee4cfa37f4a3e8df36b3092a9f651f
Heinrich Fenkart <hnrch02@gmail.com>
2014-09-25 15:51:44 +0200

    Use glob module for pattern support in CLI
    Fixes #85.

--- src/cli.js
+++ src/cli.js
@@ -4,27 +4,34 @@
 'use strict';
 
 var fs = require('fs');
+var glob = require('glob');
 var bootlint = require('./bootlint.js');
 
 var totalErrCount = 0;
-var filenames = process.argv.slice(2);
-filenames.forEach(function (filename) {
-    var html = null;
-    try {
-        html = fs.readFileSync(filename, {encoding: 'utf8'});
-    }
-    catch (err) {
-        console.log(filename + ":", err);
-        return;
-    }
-    var errs = bootlint.lintHtml(html);
-    totalErrCount += errs.length;
-    errs.forEach(function (msg) {
-        console.log(filename + ":", msg);
+var totalFileCount = 0;
+var patterns = process.argv.slice(2);
+patterns.forEach(function (pattern) {
+    var filenames = glob.sync(pattern);
+
+    filenames.forEach(function (filename) {
+        var html = null;
+        try {
+            html = fs.readFileSync(filename, {encoding: 'utf8'});
+        }
+        catch (err) {
+            console.log(filename + ":", err);
+            return;
+        }
+        var errs = bootlint.lintHtml(html);
+        totalErrCount += errs.length;
+        totalFileCount++;
+        errs.forEach(function (msg) {
+            console.log(filename + ":", msg);
+        });
     });
 });
 
-console.log("" + totalErrCount + " lint errors found across " + filenames.length + " files.");
+console.log("" + totalErrCount + " lint errors found across " + totalFileCount + " files.");
 if (totalErrCount) {
     process.exit(1);
 }
