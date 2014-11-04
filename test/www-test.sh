f84f853a925c6fb09eb63a700b2f73d03ba27582
Chris Rebert <code@rebertia.com>
2014-11-03 20:01:08 -0800

    don't leak a server process in the test script

--- test/www-test.sh
+++ test/www-test.sh
@@ -2,3 +2,4 @@
 ../bin/www &
 sleep 5
 curl -H "Content-Type: text/html; charset=UTF-8" -H "Accept: application/json" --data-binary @./fixtures/grid/cols-redundant.html  http://localhost:7070/
+kill -15 %1
