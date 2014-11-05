5152662079ed6fcbd3d9fce80265ca739ef3de97
Chris Rebert <github@rebertia.com>
2014-11-04 22:57:34 -0800

    name docker container

--- setup_droplet.sh
+++ setup_droplet.sh
@@ -18,4 +18,4 @@ ufw status verbose
 # setup Docker; written against Docker v1.0.0
 docker build . 2>&1 | tee docker.build.log
 IMAGE_ID="$(tail -n 1 docker.build.log | cut -d ' ' -f 3)"
-docker run -d -p 127.0.0.1:7070:7070 $IMAGE_ID
+docker run -d -p 127.0.0.1:7070:7070 --name bootlint $IMAGE_ID
