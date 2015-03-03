12c21b23e1fa65336a148df07c054ca97fb308ff
Kevin Kirsche <kkirsche@ansfederal.com>
2015-03-03 16:13:05 -0500

    Begin reworking tests based on @hnrch02's tips
    Begin reworking tests based on @hnrch02's tips

--- test/cli_test.js
+++ test/cli_test.js
@@ -3,7 +3,9 @@
 
 'use strict';
 
-var cli = (process.env.BOOTLINT_COV === '1') ? require('../src-cov/cli.js') : require('../src/cli.js');
+var sinon = require('sinon');
+var rewire = require('rewire');
+var cli = (process.env.BOOTLINT_COV === '1') ? rewire('../src-cov/cli.js') : rewire('../src/cli.js');
 
 /*
     ======== A Handy Little Nodeunit Reference ========
@@ -22,67 +24,62 @@ var cli = (process.env.BOOTLINT_COV === '1') ? require('../src-cov/cli.js') : re
         test.doesNotThrow(block, [error], [message])
         test.ifError(value)
 */
+var startingDir = process.cwd();
+var oldTTY = null;
+
+function rAfter() {
+    if (process.stdout.write.restore) {
+        process.stdout.write.restore();
+    }
+
+    if (process.stderr.write.restore) {
+        process.stderr.write.restore();
+    }
+}
 
 exports.bootlint = {
     setUp: function (done) {
-        // setup here
+        oldTTY = process.stdin.isTTY;
+
+        sinon.stub(process.stdout, 'write');
+        sinon.stub(process.stderr, 'write');
         done();
     },
-    'Program details': function (test) {
-        test.expect(3);
-        test.strictEqual(cli.program.version(), '0.11.0', 'should give the current Bootlint version.');
-        test.strictEqual(cli.program.description(), 'Lint the HTML of Bootstrap projects', 'should provide a description of Bootlint.');
-        test.strictEqual(cli.program.usage(), '[options] [files...]', 'should tell how to use Bootlint.');
-        test.done();
+    tearDown: function (done) {
+        process.stdin.isTTY = oldTTY;
+        process.chdir(startingDir);
+
+        // If stdin rewrites were not used, restore them here
+        rAfter();
+        done();
     },
-    'Disabled options': function (test) {
-        test.expect(5);
-        test.strictEqual(cli.program.option('')._allowUnknownOption, false, 'Don\'t allow unknown command line options.');
-        test.strictEqual(cli.program.option('')._name, 'grunt', 'should list the program\'s name.');
-        test.strictEqual(cli.program.option('')._version, '0.11.0', 'should list the program\'s version.');
-        test.strictEqual(cli.program.option('')._description, 'Lint the HTML of Bootstrap projects',
-            'should provide a description of Bootlint.');
-        test.strictEqual(cli.program.option('')._usage, '[options] [files...]', 'should provide Bootlint usage options.');
-        test.done();
-    },
-    'Version flag': function (test) {
-        test.expect(7);
-        test.strictEqual(cli.program.option('').options[0].flags, '-V, --version', 'should list the version flag');
-        test.strictEqual(cli.program.option('').options[0].required, 0, 'should not require the version flag.');
-        test.strictEqual(cli.program.option('').options[0].optional, 0, 'should return whether the version flag is optional.');
-        test.strictEqual(cli.program.option('').options[0].bool, true, 'should return whether the version flag is optional.');
-        test.strictEqual(cli.program.option('').options[0].short, '-V', 'should list the short version flag');
-        test.strictEqual(cli.program.option('').options[0].long, '--version', 'should list the long version flag');
-        test.strictEqual(cli.program.option('').options[0].description, 'output the version number',
-            'should provide a description of the version flag');
-        test.done();
-    },
-    'Disabled flag': function (test) {
-        test.expect(7);
-        test.strictEqual(cli.program.option('').options[1].flags, '-d, --disable <IDs>', 'should list the disable flag');
-        test.strictEqual(cli.program.option('').options[1].required, -15, 'should not require the disabled flag.');
-        test.strictEqual(cli.program.option('').options[1].optional, 0, 'should return whether the version flag is optional.');
-        test.strictEqual(cli.program.option('').options[1].bool, true, 'should return whether the version flag is optional.');
-        test.strictEqual(cli.program.option('').options[1].short, '-d', 'should list the short disable flag');
-        test.strictEqual(cli.program.option('').options[1].long, '--disable', 'should list the long disable flag');
-        test.strictEqual(cli.program.option('').options[1].description, 'Comma-separated list of disabled lint problem IDs',
-            'should provide a description of the disable flag');
-        test.done();
-    },
-    'Handle standard input': function (test) {
-        test.expect(6);
-        test.strictEqual(cli.handleStdin('-d')._bitField, 268435456, 'Should calculate bitfield of argument');
-        test.strictEqual(cli.handleStdin('-d')._fulfillmentHandler0, undefined, 'Should leave fulfillment handler as undefined.');
-        test.strictEqual(cli.handleStdin('-d')._rejectionHandler0, undefined, 'Should leave rejection handler as undefined');
-        test.strictEqual(cli.handleStdin('-d')._promise0, undefined, 'Should leave promise undefined.');
-        test.strictEqual(cli.handleStdin('-d')._receiver0, undefined, 'Should leave receiver as undefined');
-        test.strictEqual(cli.handleStdin('-d')._settledValue, undefined, 'Should leave settled value as undefined.');
-        test.done();
-    },
-    'Build reporter': function (test) {
-        test.expect(1);
-        var reporter = cli.buildReporter('<stdin>');
-        test.strictEqual(typeof reporter, 'function', 'Reporter should be a function');
+    'Disable tags': function (test) {
+        var i = 0;
+
+        sinon.stub(console, 'log', function (message) {
+            switch (i) {
+                case 0: {
+                    test.strictEqual(message, '');
+                    break;
+                }
+                case 1: {
+                    test.strictEqual(message, '0 lint error(s) found across 0 file(s).');
+                    break;
+                }
+            }
+
+            i++;
+        });
+
+        cli.__set__('process', {
+            argv: [''],
+            stdin: {
+                isTTY: process.stdin.isTTY
+            }
+        });
+
+        cli();
+
         test.done();
     }
 };
