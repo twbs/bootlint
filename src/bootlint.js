a1cc93c4c3cd75429b6c8894da4e1f8758b83e17
Henrik Gemal <henrik@gemal.dk>
2016-07-11 12:04:43 +0200

    Fixes #383

--- src/bootlint.js
+++ src/bootlint.js
@@ -326,7 +326,10 @@ var LocationIndex = _location.LocationIndex;
         var meta = $('head>meta[charset]');
         var charset = meta.attr('charset');
         if (!charset) {
-            reporter('`<head>` is missing UTF-8 charset `<meta>` tag');
+            meta = $('head>meta[http-equiv="Content-Type"][content="text/html; charset=utf-8"]');
+            if (!meta.length) {
+                reporter('`<head>` is missing UTF-8 charset `<meta>` tag');
+            }
         }
         else if (charset.toLowerCase() !== "utf-8") {
             reporter('charset `<meta>` tag is specifying a legacy, non-UTF-8 charset', meta);
