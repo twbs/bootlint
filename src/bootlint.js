79ca1090de40780f85d06a51e471710ab0d0fb77
Herst <Herst@users.noreply.github.com>
2018-12-26 22:57:49 +0100

    Container-related checks
    Port E003 (misplaced .row), and remove E004
    (nestable containers).

--- src/bootlint.js
+++ src/bootlint.js
@@ -586,7 +586,6 @@ var LocationIndex = _location.LocationIndex;
             }
         };
     })());
-    /*
     addLinter('E003', function lintContainers($, reporter) {
         var notAnyColClass = COL_CLASSES.map(function (colClass) {
             return ':not(' + colClass + ')';
@@ -607,15 +606,6 @@ var LocationIndex = _location.LocationIndex;
             reporter('Found one or more `.row`s that were not children of a grid column or descendants of a `.container` or `.container-fluid` or `.modal-body`', rowsOutsideColumnsAndContainers);
         }
     });
-    */
-    /*
-    addLinter('E004', function lintNestedContainers($, reporter) {
-        var nestedContainers = $('.container, .container-fluid').children('.container, .container-fluid');
-        if (nestedContainers.length) {
-            reporter('Containers (`.container` and `.container-fluid`) are not nestable', nestedContainers);
-        }
-    });
-    */
     addLinter('E005', function lintRowAndColOnSameElem($, reporter) {
         var selector = COL_CLASSES.map(function (col) {
             return '.row' + col;
