fd2eef9d5890880f394c84e9768ccdaeb5271bd5
XhmikosR <xhmikosr@gmail.com>
2018-12-28 16:55:01 +0200

    Update QUnit to v2.4.1.

--- test/fixtures/generic-qunit.js
+++ test/fixtures/generic-qunit.js
@@ -16,10 +16,10 @@
     QUnit.test(window.location.pathname, function (assert) {
         // Remove checkboxes QUnit dynamically adds to the DOM as part of its UI,
         // because these checkboxes do not comply with some Bootlint checks.
-        $('.qunit-filter').remove();
+        $('.qunit-filter, #qunit-modulefilter, #qunit-abort-tests-button').remove();
 
         assert.expect(1);
-        var lints = Array.prototype.slice.call(document.querySelectorAll('#bootlint>li'));
+        var lints = Array.prototype.slice.call(document.querySelectorAll('#bootlint > li'));
         var expectedLintMsgs = lints.map(function (item) {
             return item.dataset.lint;
         });
